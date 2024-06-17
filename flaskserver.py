71b80afcece53b84770bcfd3d3ac6efde99f43ce
Ethan Sharma <esharma50@stuy.edu>
2024-06-17 18:51:25 -0400

    added regression code

--- flaskserver.py
+++ flaskserver.py
@@ -1,4 +1,58 @@
 from flask import Flask, request, jsonify, render_template
+import numpy as np
+pilocations = {"Device1": [.02, .02, .02], "Device2" : [.03, .03, .03], "Device3" : [.04, .04, .04], "Device4" : [.05, .05, .05]}
+
+
+
+def regression(rssi): #TO-DO
+    xdata = pilocations[] #
+    ydata = pilocations[] #
+    zdata = pilocations[] #
+
+    Bdata = []
+    Cdata = []
+    Ddata = []
+
+    distdata = []
+    bvectdata = []
+    #However the data is formatted CHANGE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+    
+    #print(data)
+    for i in range(len(data)):
+    datapoint = data[i].split(" ")
+    a = float(datapoint[0])
+    b = float(datapoint[1])
+    c = float(datapoint[2])
+    d = float(datapoint[3])
+    xdata.append(a)
+    ydata.append(b)
+    zdata.append(c)
+    Bdata.append(-2 * a)
+    Cdata.append(-2 * b)
+    Ddata.append(-2 * c)
+    distdata.append(d)
+    bvectdata.append(d ** 2 - c ** 2 - b ** 2 - a ** 2)
+    #However the data is formatted CHANGE THIS!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+
+    one = np.ones(len(xdata), dtype=float)
+    M = np.array([one, Bdata, Cdata, Ddata])
+    y = np.array(bvectdata)
+    y = y.transpose()  # vertical
+    M = M.transpose()  # vertical
+    try:
+        N = np.linalg.solve(M, y)
+    except:
+        K = np.linalg.pinv(M)
+        N = K @ y
+
+
+    def eval_error():
+        return np.linalg.norm(M @ N - y) ** 2
+
+    return [N[1], N[2], N[3], eval_error()]
+
+
 
 app = Flask(__name__)
 
@@ -28,7 +82,8 @@ def receive_rssi_data():
 
 @app.route('/getData', methods=['GET'])
 def get_data():
-    return jsonify({'status': 'success', 'data': recieved_rssi}), 200
+    regression_result = regression(recieved_rssi)
+    return jsonify({'status': 'success', 'data': regression_result}), 200
 
 
 @app.route('/')
